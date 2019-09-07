4a7bbe91695e7e5b14d7e7553a7e1e41ac295e78
Liam Dawson <liamrdawson@gmail.com>
2019-09-07 23:46:33 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './App.css';
+import SearchForm from './Components/SearchForm';
+import GifList from './Components/GifList';
+
+export default class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      gifs: []
+    };
+  } 
+
+  componentDidMount() {
+    fetch()
+  }
+
+  render() { 
+    return (
+      <div>
+        <div className="main-header">
+          <div className="inner">
+            <h1 className="main-title">GifSearch</h1>
+            <SearchForm />      
+          </div>   
+        </div>    
+        <div className="main-content">
+          <GifList />
+        </div>
+      </div>
+    );
+  }
+}
