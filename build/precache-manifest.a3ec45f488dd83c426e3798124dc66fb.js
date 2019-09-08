105d660d74c89f38f1f90eb50206b27a90a78dcd
Liam Dawson <liamrdawson@gmail.com>
2019-09-08 13:48:23 +0100

    add build folder

---
+++ build/precache-manifest.a3ec45f488dd83c426e3798124dc66fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98c048788335c8a89b95fc56c0716363",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62b145b710fd2c3903ff",
+    "url": "/static/css/main.ad86e12b.chunk.css"
+  },
+  {
+    "revision": "9975621fa0145a87de51",
+    "url": "/static/js/2.2814cc33.chunk.js"
+  },
+  {
+    "revision": "62b145b710fd2c3903ff",
+    "url": "/static/js/main.4275ee3b.chunk.js"
+  },
+  {
+    "revision": "68082279f5c354e289c5",
+    "url": "/static/js/runtime~main.a6037c50.js"
+  }
+]);
